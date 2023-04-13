5daedbb922839cae10fbccae9a5d619fd7c6da38
Kaio Marques Fonseca <kaio.fonseca@codeplan.df.gov.br>
2023-04-13 10:51:42 -0300

    Tipos básicos

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,24 @@
 "use strict";
+{ }
 let age = 5;
-console.log(age);
+const firstName = 'Kaio';
+const isValid = true;
+let idk = 5;
+const ids = [1, 2, 3, 4, 5];
+const booleans = [true, false, true, false];
+const names = ['Kaio', 'Bea'];
+const person = [1, 'k'];
+const people = [
+    [1, 'Bea'],
+    [2, 'Doe'],
+];
+const productId = 1;
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+})(Direction || (Direction = {}));
+const direction = Direction.Up;
+console.log(direction);
+const productName = 'Boné';
+let itemId = productName;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
+{/* ------ TIPOS BÁSICOS ------ */}
+
 let age : number = 5
-console.log(age)
+const firstName : string = 'Kaio'
+const isValid : boolean = true
+let idk : any = 5 // basicamente tira a magia do ts
+
+const ids : number[] = [1, 2, 3, 4, 5] // Isso fala pro js que vai ser uma lista de numeros
+const booleans : boolean[] = [true, false, true, false]
+const names : string[] = ['Kaio', 'Bea']
+
+// TUPLA - Um tipo de variável que tenha um determinado tipo de valor em uma determinada ordem. Garantir uma ordem
+const person : [number, string] = [1, 'k']
+// Lista de tuplas
+const people : [number, string][] = [
+    [1, 'Bea'],
+    [2, 'Doe'],
+]
+
+// INTERSECTIONS - Situações que a gnt quer que a variável possua um tipo ou outra
+const productId : string | number = 1
+
+// ENUM - Bom em situações em que tem umvalor que sempre vai ser atribuid a outro valor.
+enum Direction {
+    Up = 1,
+    Down = 2,   
+}
+
+const direction = Direction.Up // essa variável vai ser 1 
+console.log(direction)
+
+// TYPE ASSERTIONS - quando a gnt quet mudar o tipo da variável, trata uma variável que é de um determinado tipo diferente.
+const productName : any = 'Boné'
+// let itemId = productName as string
+let itemId = <string>productName 
+
+ 
